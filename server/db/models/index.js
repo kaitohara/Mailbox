6de0de8f4817750364f2206c13547027554da4d9
GregAMeyer <gregmeyer888@gmail.com>
2015-08-13 13:56:06 -0400

    user handle and inbound schemas added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,3 +2,9 @@
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
 require('./user');
+require('./handle');
+require('./team');
+require('./inbound'); // ?
+require('./outbound'); // ?
+require('./handle');
+require('./conversation');
