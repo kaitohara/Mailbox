10c47552a1d58fcf98fc8a2d4c7e1c5031404d8b
GregAMeyer <gregmeyer888@gmail.com>
2015-08-14 19:35:59 -0400

    can get the threads and display them for the req.user

--- server/app/routes/google/index.js
+++ server/app/routes/google/index.js
@@ -3,14 +3,16 @@ module.exports = router;
 var _ = require('lodash');
 var requestPromise = require('request-promise');
 
-console.log('loading')
-// console.log(req.user)
 
 router.get('/getAllEmails', function(req, res){
-	console.log('req.user is:', req.user.accessToken)
-	requestPromise.get('https://www.googleapis.com/gmail/v1/users/me/messages', {headers: {'Authorization': 'Bearer '+req.user.accessToken}})
-		.then(function(res){
-			console.log(res)
-			// res.status(200).send('I broke')
+	console.log('hitting getAllEmails route')
+	console.log('in getAllEmails route, req.user is:', req.user.accessToken)
+	requestPromise.get('https://www.googleapis.com/gmail/v1/users/me/messages?maxResults=10', {headers: {'Authorization': 'Bearer '+req.user.accessToken} })
+		.then(function(threads){
+			console.log('went to google api and requested req.users threads: ', threads)
+			res.send(threads)
 	})
 })
+
+
+//https://www.googleapis.com/gmail/v1/users//threads?maxResults=12&key={YOUR_API_KEY}
