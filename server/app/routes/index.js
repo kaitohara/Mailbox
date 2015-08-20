d98e332a011205a238eedc23e08982579511ca2f
Robinnnnn <rokim8@gmail.com>
2015-08-19 21:32:02 -0400

    thread assignment v1 working

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,9 +6,10 @@ router.use('/members', require('./members'));
 router.use('/google', require('./google'));
 router.use('/teams', require('./teams'));
 router.use('/users', require('./users'));
+router.use('/threads', require('./threads'));
 
 // Make sure this is after all of
 // the registered routes!
-router.use(function (req, res) {
-    res.status(404).end();
+router.use(function(req, res) {
+	res.status(404).end();
 });
---
+++ server/app/routes/threads/index.js
@@ -0,0 +1,26 @@
+var router = require('express').Router();
+module.exports = router;
+
+var mongoose = require('mongoose');
+var ThreadModel = mongoose.model('Thread');
+var UserModel = mongoose.model('User');
+
+router.post('/assign/', function(req, res) {
+	var foundThread;
+
+	ThreadModel.findById(req.body.threadId)
+		.then(function(thread) {
+			foundThread = thread;
+		})
+		.then(function() {
+			UserModel.findById(req.body.assignedTo)
+				.then(function(user) {
+					foundThread.assignedTo = user;
+					foundThread.save();
+					console.log(foundThread)
+				})
+		})
+		.then(function() {
+			res.send(200);
+		})
+})
