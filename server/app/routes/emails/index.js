e5708054714da97743161ba86c5cc762a2ea6c3b
Joe <jwrice@gmail.com>
2015-08-20 15:35:06 -0400

    Sent messages via gmail api. Finally

--- server/app/routes/emails/index.js
+++ server/app/routes/emails/index.js
@@ -15,15 +15,23 @@ var sendEmail = function(team, encodedString) {
 	var emailUrl = team.email[latestEmailIndex].address.replace('@', '%40')
 	// update this to get correct token, not necessarily most recent
 	var aToken = team.email[latestEmailIndex].accessToken;
-	var options = {
-		'headers':{'Authorization': 'Bearer ' + aToken},
-		'message':{'raw': encodedString}
+	var headers = {
+		'Authorization': 'Bearer ' + aToken,
+		'Content-Type': 'application/json'
 	}
+	var options = {
+	    method: "POST",
+	    // contentType: "message/rfc822",
+	    // callbackURL: 'http://localhost:1337/callback',
+	    headers: headers,
+	    body: JSON.stringify({"raw": encodedString})
+	  };
 	var urlHead = 'https://www.googleapis.com/gmail/v1/users/'
 	var urlTail = '/messages/send'
 	var fullUrl = urlHead + emailUrl + urlTail
 	console.log(' fullurl ', fullUrl)
-	return requestPromise.post(fullUrl, options)
+	options.url = fullUrl
+	return requestPromise(fullUrl, options)
 }
 
 router.post('/sendemail/:threadId', function(req, res, next) {
@@ -31,14 +39,24 @@ router.post('/sendemail/:threadId', function(req, res, next) {
 	var associatedEmail = email.associatedEmail
 	TeamModel.findOne({'email.address': associatedEmail})
 	.then(function(team) {
-		var unencodedString = "From: <"+email.emailReply.from+"> To: <" + email.emailReply.to+ "> Subject: " + email.emailReply.subject + " Date: Fri, 21 Nov 1997 09:55:06 -0600 " + " Message-ID: <random Id> " + email.emailReply.body
-		var encodedString = base64.encode(unencodedString).replace("==","")
-		while ((encodedString.indexOf("+")>-1) || (encodedString.indexOf("/")>-1)){
-			encodedString = encodedString.replace("+","-")
-			encodedString = encodedString.replace("/","_")
-		}
-		console.log(' encoded string: ', encodedString)
-		return sendEmail(team, encodedString)
+		var otherUnencodedString = ""+
+		"Content-Type: text/plain; charset=\"UTF-8\"\n" +
+        "MIME-Version: 1.0\n" +
+        "Content-Transfer-Encoding: 7bit\n" +
+        "to: " + email.emailReply.to+"\n" +
+        "from: " + email.emailReply.from+"\n" +
+        "Reply-To: " + email.emailReply.from+"\n" +
+        "subject: " + email.emailReply.subject + "\n\n" +
+        email.emailReply.body
+        console.log(' unencodedString ', otherUnencodedString)
+        var otherEncodedString = base64.encode(otherUnencodedString).replace(/\+/g, '-').replace(/\//g, '_')
+
+
+		// var unencodedString = "From: <"+email.emailReply.from+"> To: <" + email.emailReply.to+ "> Subject: " + email.emailReply.subject + " Date: Fri, 21 Nov 1997 09:55:06 -0600 " + " Message-ID: <random Id> " + email.emailReply.body
+		// var encodedString = base64.encode(unencodedString).replace(/\+/g, '-').replace(/\//g, '_')
+		// var encodedString = encodedString
+		// console.log(' encoded string: ', encodedString)
+		return sendEmail(team, otherEncodedString)
 	}, function(err){
 		// mongoose error:
 		console.log('mongoose err:', err)
