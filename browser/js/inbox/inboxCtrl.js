fdd6a901b35cd33ca661fd90f1e8299b572101f6
GregAMeyer <gregmeyer888@gmail.com>
2015-08-21 15:31:41 -0400

    cleaned up

--- browser/js/inbox/inboxCtrl.js
+++ browser/js/inbox/inboxCtrl.js
@@ -2,7 +2,6 @@ app.controller('inboxCtrl', function($scope, $state, threads) {
 
 	$scope.threads = threads;
 
- 	// WHY WON"T THIS RUN FROM THE NG_CLICK ON A THREAD?
     $scope.goToThread = function(threadId){
 		console.log('going to thread', threadId)
 		$state.go('home.teamId.threadId', {threadId: threadId})
