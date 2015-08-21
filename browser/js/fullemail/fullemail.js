65c724707c6c45df403dd88027fcdf6b43fe8db4
GregAMeyer <gregmeyer888@gmail.com>
2015-08-21 16:18:57 -0400

    grabbed history, cleaner version of front end routing

--- browser/js/fullemail/fullemail.js
+++ browser/js/fullemail/fullemail.js
@@ -2,11 +2,13 @@ app.directive('fullemail', function($rootScope, AuthService, AUTH_EVENTS, $state
 
 	return {
 		restrict: 'E',
-		templateUrl: 'js/fullemail/fullemail.html',
-		controller: 'fullemailCtrl',
-		scope: {
-			thread: '='
-		}
+		templateUrl: 'js/fullemail/fullemail.html'
+		// controller: 'fullemailCtrl',
+		// scope: {
+		// 	thread: '='
+		// }
+		// dont need controlelr here because we set controller in the state
+		// dont need scope here because we set scope via resolve in fullemailCtrl
 	};
 
 });
--- browser/js/inbox/inbox.js
+++ browser/js/inbox/inbox.js
@@ -2,11 +2,13 @@ app.directive('inbox', function($rootScope, AuthService, AUTH_EVENTS, $state) {
 
 	return {
 		restrict: 'E',
-		templateUrl: 'js/inbox/inbox.html',
-		controller: 'inboxCtrl',
-		scope: {
-			threads: '='
-		}
+		templateUrl: 'js/inbox/inbox.html'
+		// controller: 'inboxCtrl',
+		// scope: {
+		// 	threads: '='
+		// }
+		// dont need controlelr here because we set controller in the state
+		// dont need scope here because we set scope via resolve in fullemailCtrl
 	};
 
 });
