6588d95c6926291ffdd7e25b1d4af6be61bd7274
Kaito Hara <kaito.hara@me.com>
2015-08-20 22:41:17 -0400

    reply template's to field is now pre-populated

--- browser/js/fullemail/fullemailCtrl.js
+++ browser/js/fullemail/fullemailCtrl.js
@@ -1,4 +1,4 @@
-app.controller('fullemailCtrl', function($scope) {
+app.controller('fullemailCtrl', function($scope, $sce) {
 	$scope.extractField = function(messageObj, fieldName) {
 		return messageObj.googleObj.payload.headers.filter(function(header) {
 			return header.name === fieldName;
@@ -6,7 +6,10 @@ app.controller('fullemailCtrl', function($scope) {
 	};
 	$scope.parseBody = function(body){
 		var regex = /On [A-z]{3}, [A-z]{3} [0-9]{1,2}, [0-9]{4} at [0-9]{1,2}:[0-9]{1,2} [A,P]M, /;
+		// $scope.myHTML = body;
 		if (body) return (body.split(regex))[0];
 		return body;
-	}
+	};
+
+	// $scope.html = $sce.trustAsHtml($scope.someHtml);
 })
