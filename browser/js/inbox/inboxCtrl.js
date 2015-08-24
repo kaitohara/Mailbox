ae378f8cab8e2d675561eb2ecca0513dc2550908
GregAMeyer <gregmeyer888@gmail.com>
2015-08-24 11:38:37 -0400

    users and online status shows correctly upon going to state without clicking on team but logout doesnt work correctly now

--- browser/js/inbox/inboxCtrl.js
+++ browser/js/inbox/inboxCtrl.js
@@ -3,7 +3,6 @@ app.controller('inboxCtrl', function($scope, $state, threads, Socket) {
 	$scope.threads = threads;
 
 	$scope.goToThread = function(threadId) {
-		console.log('going to thread', threadId)
 		$state.go('home.teamId.threadId', {
 			threadId: threadId
 		})
