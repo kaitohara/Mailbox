4a3f34ca9fc173efa49b57a669f67cc369c5808c
Joe <jwrice@gmail.com>
2015-08-23 20:40:37 -0400

    Inline replies

---
+++ browser/js/betterreply/replyCtrl.js
@@ -0,0 +1,3 @@
+app.controller('replyCtrl', function($scope, replyFactory) {
+	
+})
---
+++ browser/js/socket/socketFactory.js
@@ -0,0 +1,22 @@
+app.factory('socketFactory', function($rootScope) {
+    var socket = io.connect()
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args)
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, function(){
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args)
+                });
+            });
+        }
+    };
+
+});
