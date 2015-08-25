166d257d2446780080203d04792979b10769f748
Robinnnnn <rokim8@gmail.com>
2015-08-25 10:30:28 -0400

    update

--- browser/js/inbox/inboxCtrl.js
+++ browser/js/inbox/inboxCtrl.js
@@ -6,19 +6,19 @@ app.controller('inboxCtrl', function($rootScope, $scope, $state, threads, Socket
 	console.log('threads', threads)
 
 	$rootScope.$on('threadAssignment', function(event, assignedThread) {
-		console.log('the assigned thread is:', assignedThread)
+		// console.log('the assigned thread is:', assignedThread)
 		console.log('all the threads', threads)
 		$scope.threads.forEach(function(thread) {
-			console.log('threadId', thread._id)
-			console.log('assignedThreadId', assignedThread._id)
+			// console.log('threadId', thread._id)
+			// console.log('assignedThreadId', assignedThread._id)
 			if (thread._id === assignedThread._id) {
 				console.log('individual thread', thread)
-				console.log('middle column thread assignment', thread.assignedTo)
-				console.log('right column thread assignment', assignedThread.assignedTo.firstName)
+					// console.log('middle column thread assignment', thread.assignedTo)
+					// console.log('right column thread assignment', assignedThread.assignedTo.firstName)
 					// thread.assignedTo = assignedThread.assignedTo.firstName;
-				$scope.threads[0].assignedTo = 'please work';
-				// $scope.$digest()
-				console.log($scope.threads)
+					// $scope.threads[0].assignedTo = 'please work';
+					// $scope.$digest()
+					// console.log($scope.threads)
 					// $scope.$apply(function() {
 					// 	thread.assignedTo = assignedThread.assignedTo.firstName;
 					// })
