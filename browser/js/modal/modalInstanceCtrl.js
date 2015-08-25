83b88e7317f68ae156371bb0d3a743d3482daa53
Joe <jwrice@gmail.com>
2015-08-25 16:06:55 -0400

    Adding modal changes

--- browser/js/modal/modalInstanceCtrl.js
+++ browser/js/modal/modalInstanceCtrl.js
@@ -1,7 +1,15 @@
-app.controller('ModalInstanceCtrl', function($scope, $modalInstance, team, users, teamFactory, $window) {
+app.controller('ModalInstanceCtrl', function($scope, $modalInstance, team, users, user, teamFactory, $window) {
 
     $scope.team = team;
     $scope.users = users;
+    $scope.user = user;
+    teamFactory.getUserTeams(user._id)
+        .then(function(user){
+            $scope.teams = user.data.teams
+            console.log($scope.teams)
+        }, function(err){
+            console.log(err)
+        })
     // $scope.selectedUser = null;
 
     $scope.chooseUser = function(user) {
@@ -22,5 +30,26 @@ app.controller('ModalInstanceCtrl', function($scope, $modalInstance, team, users
             $window.location.href = "/auth/google/team/" + $scope.email;
         })
     }
+
+    $scope.showTeamMembers = true
+
+    $scope.openMyProfile = function(){
+        $scope.showMyProfile = true
+        $scope.showTeamMembers = false
+        $scope.showTeamForm = false
+    } 
+
+    $scope.openTeamMembers = function(){
+        $scope.showMyProfile = false
+        $scope.showTeamMembers = true
+        $scope.showTeamForm = false
+    } 
+
+    $scope.openTeamForm = function(){
+        $scope.showMyProfile = false
+        $scope.showTeamMembers = false
+        $scope.showTeamForm = true
+    }
+
 
 });
