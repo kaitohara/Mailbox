67cef4d667d4ad4e801ca9bb5e6a4b9fba720531
Robinnnnn <rokim8@gmail.com>
2015-08-24 22:02:03 -0400

    trying to update assignment

--- browser/js/fullemail/fullemailCtrl.js
+++ browser/js/fullemail/fullemailCtrl.js
@@ -1,77 +1,79 @@
 app.controller('fullemailCtrl', function($scope, thread, threadFactory, $location, $anchorScroll, $rootScope, $state, $firebaseArray) {
 
-	$scope.thread;
-	$scope.assignedTo = thread.assignedTo ? thread.assignedTo.firstName : 'Assign';
-	$scope.assignedBy = thread.assignedBy ? thread.assignedBy.firstName : null;
+    $scope.thread;
+    $scope.assignedTo = thread.assignedTo ? thread.assignedTo.firstName : 'Assign';
+    $scope.assignedBy = thread.assignedBy ? thread.assignedBy.firstName : null;
 
-	$scope.gotoBottom = function() {
-		$scope.thread = thread;
-		// // set the location.hash to the id of the element
-		// $location.hash('bottom');
-		// // call $anchorScroll()
-		// $anchorScroll();
-		setTimeout(function(){
-    		$location.hash('bottom');
-      		$anchorScroll();
-  		} , 100);
+    $scope.gotoBottom = function() {
+        $scope.thread = thread;
+        // // set the location.hash to the id of the element
+        // $location.hash('bottom');
+        // // call $anchorScroll()
+        // $anchorScroll();
+        setTimeout(function() {
+            $location.hash('bottom');
+            $anchorScroll();
+        }, 100);
     };
 
     $scope.gotoBottom()
 
-	$scope.extractField = function(messageObj, fieldName) {
-		return messageObj.googleObj.payload.headers.filter(function(header) {
-			return header.name === fieldName;
-		})[0];
-	};
+    $scope.extractField = function(messageObj, fieldName) {
+        return messageObj.googleObj.payload.headers.filter(function(header) {
+            return header.name === fieldName;
+        })[0];
+    };
 
-	$scope.parseBody = function(body) {
-		var regex = /On [A-z]{3}, [A-z]{3} [0-9]{1,2}, [0-9]{4} at [0-9]{1,2}:[0-9]{1,2} [A,P]M, /;
-		if (body) return (body.split(regex))[0];
-		return body;
-	};
+    $scope.parseBody = function(body) {
+        var regex = /On [A-z]{3}, [A-z]{3} [0-9]{1,2}, [0-9]{4} at [0-9]{1,2}:[0-9]{1,2} [A,P]M, /;
+        if (body) return (body.split(regex))[0];
+        return body;
+    };
 
-	$scope.assign = function(userChoice, thread, user) {
-		// $scope.assignedUser = userChoice.firstName;
-		threadFactory.assignUserToThread(userChoice._id, thread._id, user._id)
-		.then(function(thread){
-			$scope.assignedTo = thread.data.assignedTo.firstName;
-			console.log('assigned to and rootscope user', $scope.thread, $rootScope.user)
-			if ($scope.thread.assignedTo && ($scope.thread.assignedTo._id === $rootScope.user._id)) {
-				$state.go('home.userId', 
-					{userId: $rootScope.user._id}, 
-					{reload:true});
-			}
-		});
-	};
+    $scope.assign = function(userChoice, thread, user) {
+        threadFactory.assignUserToThread(userChoice._id, thread._id, user._id)
+            .then(function(thread) {
+                $scope.assignedTo = thread.data.assignedTo.firstName;
+                if ($scope.thread.assignedTo && ($scope.thread.assignedTo._id === $rootScope.user._id)) {
+                    $state.go('home.userId', {
+                        userId: $rootScope.user._id
+                    }, {
+                        reload: true
+                    });
+                }
+                console.log('thread from step 1', thread)
+                $rootScope.$emit('threadAssignment', thread.data)
+            });
+    };
 
-	$scope.showReply = function(index){
-		console.log(index)
-		var length = $scope.thread.messages.length 
-		$scope.thread.messages[index].showReply = !$scope.thread.messages[index].showReply
-	};
+    $scope.showReply = function(index) {
+        console.log(index)
+        var length = $scope.thread.messages.length
+        $scope.thread.messages[index].showReply = !$scope.thread.messages[index].showReply
+    };
 
-	$scope.oneAtATime = false;
+    $scope.oneAtATime = false;
 
-	$scope.status = {
-    	isFirstOpen: true,
-    	isFirstDisabled: false
-	};
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
 
-	var ref = new Firebase('https://amber-fire-4541.firebaseio.com');
-	$scope.chatMessages = $firebaseArray(ref);
-    $scope.sendMessage = function(chatMessage){
-    	chatMessage.name = $scope.user.firstName;
-    	console.log(chatMessage)
+    var ref = new Firebase('https://amber-fire-4541.firebaseio.com');
+    $scope.chatMessages = $firebaseArray(ref);
+    $scope.sendMessage = function(chatMessage) {
+        chatMessage.name = $scope.user.firstName;
+        console.log(chatMessage)
         $scope.chatMessages.$add(chatMessage);
         $scope.gotoBottom()
     };
 })
 
 
-app.directive('slideable', function () {
+app.directive('slideable', function() {
     return {
-        restrict:'C',
-        compile: function (element, attr) {
+        restrict: 'C',
+        compile: function(element, attr) {
             // wrap tag
             var contents = element.html();
             element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
@@ -96,14 +98,14 @@ app.directive('slideToggle', function() {
         restrict: 'A',
         link: function(scope, element, attrs) {
             var target, content;
-            
+
             attrs.expanded = false;
-            
+
             element.bind('click', function() {
                 if (!target) target = document.querySelector(attrs.slideToggle);
                 if (!content) content = target.querySelector('.slideable_content');
-                
-                if(!attrs.expanded) {
+
+                if (!attrs.expanded) {
                     content.style.border = '1px solid rgba(0,0,0,0)';
                     var y = content.clientHeight;
                     content.style.border = 0;
@@ -116,19 +118,3 @@ app.directive('slideToggle', function() {
         }
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- browser/js/inbox/inboxCtrl.js
+++ browser/js/inbox/inboxCtrl.js
@@ -1,6 +1,35 @@
 app.controller('inboxCtrl', function($rootScope, $scope, $state, threads, Socket, teamFactory) {
 
 	$scope.threads = threads;
+	$scope.assignedTo;
+
+	console.log('threads', threads)
+
+	$rootScope.$on('threadAssignment', function(event, assignedThread) {
+		console.log('the assigned thread is:', assignedThread)
+		console.log('all the threads', threads)
+		$scope.threads.forEach(function(thread) {
+			console.log('threadId', thread._id)
+			console.log('assignedThreadId', assignedThread._id)
+			if (thread._id === assignedThread._id) {
+				console.log('individual thread', thread)
+				console.log('middle column thread assignment', thread.assignedTo)
+				console.log('right column thread assignment', assignedThread.assignedTo.firstName)
+					// thread.assignedTo = assignedThread.assignedTo.firstName;
+				$scope.threads[0].assignedTo = 'please work';
+				// $scope.$digest()
+				console.log($scope.threads)
+					// $scope.$apply(function() {
+					// 	thread.assignedTo = assignedThread.assignedTo.firstName;
+					// })
+			}
+
+			// thread.assignedTo = assignedThread.assignedTo.firstName
+			// console.log(thread.assignedTo, assignedThread.assignedTo.firstName)
+			// console.log('potentially altered thread', $scope.threads)
+		})
+	})
+
 
 	$scope.goToTeamThread = function(threadId) {
 		console.log('still hitting this')
@@ -15,25 +44,25 @@ app.controller('inboxCtrl', function($rootScope, $scope, $state, threads, Socket
 		})
 	};
 
-	$rootScope.$on('synced', function(){
+	$rootScope.$on('synced', function() {
 		console.log('heard it')
 		$scope.refreshThreads();
 	})
 
-	$scope.refreshThreads = function(){
+	$scope.refreshThreads = function() {
 		console.log('log this');
 		teamFactory.getThisTeamsGmailThreadsId($scope.team._id)
-		.then(function(threads){
-			$scope.threads = threads;
-		})
+			.then(function(threads) {
+				$scope.threads = threads;
+			})
 	};
 
-	$scope.cleanName = function(name){
+	$scope.cleanName = function(name) {
 		var regex = / <.+>/
 		return name.replace(regex, '')
 	}
 
-	$scope.simplifyDate = function(date){
-		return moment(date*1).format("MMM DD h:mm a")
+	$scope.simplifyDate = function(date) {
+		return moment(date * 1).format("MMM DD h:mm a")
 	}
 })
