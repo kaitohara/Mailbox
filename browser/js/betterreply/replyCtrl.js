6fa2e70a47d6c43a67500042d8ceb0a9fc260ae8
Robinnnnn <rokim8@gmail.com>
2015-08-25 17:33:52 -0400

    added seeding to package json

--- browser/js/betterreply/replyCtrl.js
+++ browser/js/betterreply/replyCtrl.js
@@ -3,7 +3,8 @@ app.controller('replyCtrl', function($scope, replyFactory) {
 	// need to revise replyFactory so that we can send reply without needing access to parent scopes
 
 	// currently the replyFactory requires the original .thread with all messages. it uses the associatedEmail property on the original .thread in order to send the email reply to the correct email address. this could be optimized
-	$scope.sendReply = function(){
+	$scope.sendReply = function() {
+		console.log('scope parent', $scope.$parent)
 		console.log('google thread id', $scope.$parent.$parent.thread.googleThreadId)
 		$scope.$parent.$parent.thread.emailReply = $scope.emailReply
 		console.log('parent scope emailyreply', $scope.$parent.$parent.thread.emailReply, $scope.emailReply)
@@ -11,7 +12,7 @@ app.controller('replyCtrl', function($scope, replyFactory) {
 		$scope.$parent.message.showReply = false
 	}
 
-	$scope.cancel = function(){
+	$scope.cancel = function() {
 		$scope.$parent.message.showReply = false
 	}
 })
