8f612870a9f0f54ed18c0bf76e57935bfc7bff3f
Robinnnnn <rokim8@gmail.com>
2015-08-27 18:54:06 -0400

    broadcasting offline

--- browser/js/sidebar/sidebarCtrl.js
+++ browser/js/sidebar/sidebarCtrl.js
@@ -32,30 +32,30 @@ app.controller('sidebarCtrl', function($scope, teamFactory, $stateParams, userFa
 			$scope.getTeamMembers($stateParams.teamId)
 		})
 
-		// Socket.on('offlineUser', function(userId) {
-		// 	$scope.$apply(function() {
-		// 		if ($scope.onlineUsers.indexOf(userId) > -1) {
-		// 			$scope.teammates.forEach(function(teammate) {
-		// 				if (teammate._id === userId) teammate.isOnline = false;
-		// 			})
-		// 		}
-		// 	})
+		Socket.on('offlineUser', function(userId) {
+			$scope.$apply(function() {
+				if ($scope.onlineUsers.indexOf(userId) > -1) {
+					$scope.teammates.forEach(function(teammate) {
+						if (teammate._id === userId) teammate.isOnline = false;
+					})
+				}
+			})
 
-		// })
+		})
 	}
 
 	$scope.showOnlineStatus();
 
-	Socket.on('offlineUser', function(userId) {
-		console.log('someone logged off!')
-		$scope.$apply(function() {
-			if ($scope.onlineUsers.indexOf(userId) > -1) {
-				$scope.teammates.forEach(function(teammate) {
-					if (teammate._id === userId) teammate.isOnline = false;
-				})
-			}
-		})
-	})
+	// Socket.on('offlineUser', function(userId) {
+	// 	console.log('someone logged off!')
+	// 	$scope.$apply(function() {
+	// 		if ($scope.onlineUsers.indexOf(userId) > -1) {
+	// 			$scope.teammates.forEach(function(teammate) {
+	// 				if (teammate._id === userId) teammate.isOnline = false;
+	// 			})
+	// 		}
+	// 	})
+	// })
 
 	$scope.setTeamActive = function(index) {
 		console.log('index', index)
